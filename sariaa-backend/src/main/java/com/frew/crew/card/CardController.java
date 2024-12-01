aec172189f973a039d5f36cf458c61a42d08691b
IlyasBlidi <blidi.ilyas19@gmail.com>
2024-12-01 22:20:01 +0100

    charge user card

--- sariaa-backend/src/main/java/com/frew/crew/card/CardController.java
+++ sariaa-backend/src/main/java/com/frew/crew/card/CardController.java
@@ -1,12 +1,13 @@
 package com.frew.crew.card;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +19,11 @@ public class CardController {
   public ResponseEntity<List<CardDTO>> getAllCards() {
     List<CardDTO> cards = cardService.getAllCards();
     return ResponseEntity.ok(cards);
+  }
+
+  @PatchMapping("/{cardId}")
+  public ResponseEntity<Card> chargeCardBalance(@PathVariable UUID cardId, @RequestBody BigDecimal amount) {
+    Card card = cardService.chargeCardBalance(cardId, amount);
+    return new ResponseEntity<>(card, HttpStatus.ACCEPTED);
   }
 }
