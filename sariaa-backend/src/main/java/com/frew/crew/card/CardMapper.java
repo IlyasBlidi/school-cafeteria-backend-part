3dab9dce7d5c04f1c2776089daf70b3bcd62b334
IlyasBlidi <blidi.ilyas19@gmail.com>
2024-12-04 13:02:22 +0100

    add the non instantiability of mappers

--- sariaa-backend/src/main/java/com/frew/crew/card/CardMapper.java
+++ sariaa-backend/src/main/java/com/frew/crew/card/CardMapper.java
@@ -3,12 +3,16 @@ package com.frew.crew.card;
 import static com.frew.crew.user.UserMapper.toUserSummaryDTO;
 
 public class CardMapper {
+  private CardMapper() {
+    // Prevent instantiation
+  }
+
   public static CardDTO toCardDTO(Card card) {
     return new CardDTO(
-        card.getId(),
-        card.getBalance(),
-        card.getLastUpdateDate(),
-        card.getUser() != null ? toUserSummaryDTO(card.getUser()) : null
+      card.getId(),
+      card.getBalance(),
+      card.getLastUpdateDate(),
+      card.getUser() != null ? toUserSummaryDTO(card.getUser()) : null
     );
   }
 
